7ce8cca984ea6631240960a4f198d5080f31048b
robbindhilipjsparkout <121787444+robbindhilipjsparkout@users.noreply.github.com>
2023-01-24 19:01:07 +0530

    Add files via upload

--- basicformjs.js
+++ basicformjs.js
@@ -11,9 +11,22 @@
         }    
     } 
 
+/*
+//resset form
 
+function removefeilds(){
 
+let column = document.createElement('div');
 
+var resset = document.getElementById("feild");
+
+resset.removeChild();
+
+column.setAttribute('id','feild');
+document.getElementById('new_feild').removeChild(column);
+
+}
+*/
 
 // dynamic form:
 
@@ -21,38 +34,56 @@ function addform(type){
 
     //
     var column = document.createElement('input');
-    
+   
     //
-    var select_feild = document.createElement('select_feild');
-    
+    var label1 = document.createElement("textlabel");
+    //
+    var label2 = document.createElement("textlabel");
     // 
     var append_inside = document.getElementById("feild");
+
+    //
+    var add_box = document.createElement('select');
+
     
+   var select_feild = document.createElement('select_feild');
     
-//switch statement:
-    
+
+
+
+
     switch (type){
         
     
     case "input":{
-    
+     label1.innerHTML='Type here';
+    label1.setAttribute('type','text');
+    label1.setAttribute('class','typehere');
     
     column.setAttribute("type",type);
     column.setAttribute("value","");
     column.setAttribute("id","input");
+
     
     append_inside.appendChild(column);
+    append_inside.appendChild(label1);
     break;
     
     }
     
     case "checkbox":{
+
+        label2.innerHTML='checkbox';
+        label2.setAttribute('type','text');
+        label2.setAttribute('class','chkbox');
+        
     
     column.setAttribute("type",type);
     column.setAttribute("value",type);
     column.setAttribute('id','checkbox');
     
     append_inside.appendChild(column);
+    append_inside.appendChild(label2);
     break;
     
     
@@ -60,38 +91,65 @@ function addform(type){
     }
     
     case "radio":{
+
+
+
+        label2.innerHTML='radiobtn';
+        label2.setAttribute('type','text');
+        label2.setAttribute('class','radiobtn');
     
     column.setAttribute('type',type);
     column.setAttribute('value',type);
     column.setAttribute('id','radio');
     
     append_inside.appendChild(column);
+    append_inside.appendChild(label2);
     break;
     }
     
     
     
+   
+        case "addbox":{
+
+add_box.innerHTML=
+
+
+"<option>form1</option><option>form2</option><option>form3</option>";
+
+add_box.setAttribute('id','selectf');
+
+label2.innerHTML = 'select_feild';
+label2.setAttribute('id','selectL');
+
+append_inside.appendChild(label2);
+append_inside.appendChild(add_box);
+break;
+        }
     
-    //  "select feild ":{
-        default:
     
-    
+default:
+
+
     select_feild.setAttribute('type',type);
-    
-    
-    //  select_feild.setAttribute ('value',"--please select any option--");
-    select_feild.innerHTML = "--please select any option--";
-    select_feild.setAttribute('id','selectfeild');
-    
-    
-    append_inside.appendChild(select_feild);
-    
+
+alert('--please select an option--')
+
+
+
     }
-    
-    }
-    
-    
-        /*
+}
+
+
+
+
+
+
+
+
+
+
+          /*
 document.querySelector('#rr').onclick = function () {
 
 var password = document.querySelector('.password').value,
